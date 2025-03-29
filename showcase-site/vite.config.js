e55d6e3907ad46350b36bf27f6dd4c291fa883b4
CWTWIS <chawanrat.wis@gmail.com>
2025-03-30 02:59:07 +1100

    Adjusted base path

--- showcase-site/vite.config.js
+++ showcase-site/vite.config.js
@@ -1,17 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  base: "/showcase-site",
   dependencies: {
-    "react": "^18.2.0",
+    react: "^18.2.0",
     "react-dom": "^18.2.0",
     "react-dnd": "^16.0.1",
     "react-dnd-html5-backend": "^16.0.1",
-    "recharts": "^2.12.0"
-  }
-})
+    recharts: "^2.12.0",
+  },
+});
