025ec81bcb2bfc9c2f3cd882a29c6b163780536a
Nelly <kchen0116@gmail.com>
2025-03-29 13:01:44 +1100

    Add basic implementation of to-do-list

--- showcase-site/src/Development/Index.jsx
+++ showcase-site/src/Development/Index.jsx
@@ -1,22 +1,48 @@
 import { useState } from 'react';
 import "./assets"
 import "./css"
-import Lists from "./"
+import TodoForm from './ToDoForm';
+import ListCard from './ListCard';
 
 const Index = () => {
-    const [toDoList, setToDoList] = useState([]);
+    const [toDoList, setToDoList] = useState(initialList);
+    const [level, setLevel] = useState(0);
+    const [taskcompleted, setTaskCompleted] = useState(0);
 
-    const handleAddOnClick = () => {
-        // Propmt user input
-        // Get user imput
-        // Create new list card
-        // Add task to toDoList
-    }
+    const initialList = [
+        { id: 'id1', title: 'Example', done: false }
+    ];
+
+    const atAddItem = (text) => {
+        const item = {
+            id: new Date().getTime().toString(),
+            text,
+            done: false,
+        };
+        setToDoList(toDoList.concat(item));
+    };
+
+    const atClickItem = (id) => {
+        const newList = toDoList.map((item) => {
+          if (item.id === id) {
+            return {
+              id: item.id,
+              title: item.title,
+              done: !item.done,
+            };
+          }
+          return item;
+        });
+        setToDoList(newList);
+      };
 
     return (
-        <div>
-            <Lists/>
-        </div>
+        <section>
+            <TodoForm onAddItem={atAddItem}/>
+            <ul>
+                {toDoList.map(toDoItem => <ListCard key={toDoItem.id} taskTitle={toDoItem.title} onClickItem={atClickItem}/>)}
+            </ul>
+        </section>
     )
 }
 
--- showcase-site/src/Development/ListCard.jsx
+++ showcase-site/src/Development/ListCard.jsx
@@ -2,11 +2,18 @@ import PropTypes from 'props-types'
 
 ListCard.propTypes = {
     taskTitle: PropTypes.string,
-    taskDateTime: PropTypes.string
+    taskDateTime: PropTypes.string,
+    onClickItem: PropTypes.func
 }
 
-const ListCard = ({taskTitle, taskDateTime}) => {  
-    return <li>{`${taskTitle}  ${taskDateTime}`}</li>
+const ListCard = (props) => {  
+    const {taskTitle, onClickItem} = props;
+
+    const atClick = () => {
+        onClickItem();
+    };
+
+    return <li onClick={atClick}>{`${taskTitle}`}</li>
 }
 
 export default ListCard
